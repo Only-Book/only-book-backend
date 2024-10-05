91ec501c57db9ae5443eaa954b93cbbaa7cc6210
jihyeonEom <jjiihyun@naver.com>
2024-10-05 12:37:55 +0900

    init setting

---
+++ src/main/java/org/myongjithon/onlybook/OnlyBookApplication.java
@@ -0,0 +1,13 @@
+package org.myongjithon.onlybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlyBookApplication.class, args);
+    }
+
+}
