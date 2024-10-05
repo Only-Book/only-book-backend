9beca4b5797c9afcd1ed25c3a1c9386720ff15a6
jihyeonEom <jjiihyun@naver.com>
2024-10-05 16:24:49 +0900

    feature: User, UserCategory Entity 추가 작성

--- src/main/java/org/myongjithon/onlybook/domain/user/entity/User.java
+++ src/main/java/org/myongjithon/onlybook/domain/user/entity/User.java
@@ -3,6 +3,10 @@ package org.myongjithon.onlybook.domain.user.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.myongjithon.onlybook.domain.BaseEntity;
+import org.myongjithon.onlybook.domain.comment.Comment;
+import org.myongjithon.onlybook.domain.usercategory.UserCategory;
+
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -19,5 +23,11 @@ public class User extends BaseEntity {
 
     @Column(nullable = false)
     private String nickname;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<UserCategory> user_category;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Comment> comments;
 
 }
--- src/main/java/org/myongjithon/onlybook/domain/usercategory/UserCategory.java
+++ src/main/java/org/myongjithon/onlybook/domain/usercategory/UserCategory.java
@@ -1,6 +1,27 @@
 package org.myongjithon.onlybook.domain.usercategory;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
 import org.myongjithon.onlybook.domain.BaseEntity;
+import org.myongjithon.onlybook.domain.category.entity.Category;
+import org.myongjithon.onlybook.domain.user.entity.User;
 
+@Entity(name = "user_category")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserCategory extends BaseEntity {
+
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
 }
